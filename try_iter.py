c3c60783bbe858a6e9649f089605a4f2a19cb126
garyyjn <garyyjn@gmail.com>
2021-12-09 02:07:22 -0500

    idk

--- try_iter.py
+++ try_iter.py
@@ -1,2 +1,6 @@
 import numpy as np
+import os
 data_path = 'home/ext_yao_gary_mayo_edu/FuseMount/datasets/TCGA_BLCA'
+
+for filename in os.listdir(data_path):
+    print(os.path.join(data_path, filename))
