f0fc5ca4fe414f671abfd44d8286218439c699de
garyyjn <garyyjn@gmail.com>
2021-12-13 13:37:10 -0500

    Update try_iter.py

--- try_iter.py
+++ try_iter.py
@@ -14,7 +14,7 @@ else:
 from models.resnet_custom import ResNet_Baseline, resnet50_baseline
 
 feature_extractor = resnet50_baseline(pretrained=True)
-'''
+
 if torch.cuda.is_available():
     feature_extractor = feature_extractor.cuda()
 for filename in tqdm(os.listdir(data_path)):
@@ -25,3 +25,4 @@ for filename in tqdm(os.listdir(data_path)):
 filename = 'datasets_TCGA_BLCA_TCGA-HQ-A5NE-01Z-00-DX1.8046B606-085B-406F-A8D7-92A3E021020A.svs'
 full_path = os.path.join(data_path, filename)
 simple_extraction(filename, full_path, output_path, feature_extractor)
+'''
