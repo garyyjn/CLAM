ef998e6c3ed7410d251ba77a58dc360fafc821ee
garyyjn <garyyjn@gmail.com>
2021-12-13 12:18:08 -0500

    Update try_iter.py

--- try_iter.py
+++ try_iter.py
@@ -16,7 +16,6 @@ from models.resnet_custom import ResNet_Baseline, resnet50_baseline
 feature_extractor = resnet50_baseline(pretrained=True)
 if torch.cuda.is_available():
     feature_extractor = feature_extractor.cuda()
-'''
 for filename in tqdm(os.listdir(data_path)):
     if filename.endswith('.svs'):
         full_path = os.path.join(data_path, filename)
@@ -25,3 +24,4 @@ for filename in tqdm(os.listdir(data_path)):
 filename = 'TCGA-HQ-A5NE-01Z-00-DX1.8046B606-085B-406F-A8D7-92A3E021020A.svs'
 full_path = os.path.join(data_path, filename)
 small_feature_extraction(filename, full_path, output_path, feature_extractor)
+'''
